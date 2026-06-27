3e36a1d5ef69e7b6d4159b4bcaf840e3bf1c6e83
gopika-jastack <gopikashree2000@gmail.com>
2026-06-27 17:40:07 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1" {
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2" {
+  default = "10.0.4.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "db_username" {
+  description = "Database username"
+}
+
+variable "db_password" {
+  description = "Database password"
+  sensitive   = true
+}
